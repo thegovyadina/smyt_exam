c11798fa0e08d8d78df66093b3c62e1523398218
x47 <paulxaoc@gmail.com>
2014-09-12 03:28:00 +0400

    Changed default date format

--- smyt_exam/settings.py
+++ smyt_exam/settings.py
@@ -63,9 +63,11 @@ TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = False
 
 USE_TZ = True
+
+DATE_FORMAT = "d.m.Y"
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
